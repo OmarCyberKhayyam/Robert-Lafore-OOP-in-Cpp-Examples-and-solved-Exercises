90a3bc934001f1727fb499dc401c6c215e444f17
OmarCyberKhayyam <148579148+OmarCyberKhayyam@users.noreply.github.com>
2024-01-25 20:51:41 +0300

    Add files via upload

---
+++ INCREM.cpp
@@ -0,0 +1,16 @@
+//increm.cpp
+//demonstrates the increment operator
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int count = 10;
+
+    cout << "count=" << count << endl;
+    cout << "count=" << ++count << endl;
+    cout << "count=" << count << endl;
+    cout << "count=" << count++ << endl;
+    cout << "count=" << count << endl;
+    return 0;
+}
